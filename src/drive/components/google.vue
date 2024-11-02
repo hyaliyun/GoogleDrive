f0d52a2d62653a4c46fffdb9cad4518e224769aa
react <hy0228@aliyun.com>
2024-11-03 07:35:15 +0800

    Add files via upload

---
+++ src/drive/components/google.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="exam-cheer">
+      btc！
+    </div>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style scoped>
+  .exam-cheer {
+    font-size: 24px;
+    color: #008000; /* 深绿色，寓意希望和成功 */
+    text-align: center;
+    margin-top: 50px;
+  }
+  </style>
